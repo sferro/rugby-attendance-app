7c9677a1ab1cdec26be1f9788cbfabf743e51c48
Sebastian Ferro <sferro@gmail.com>
2026-06-12 17:54:37 -0300

    Register player model in admin

--- apps/players/admin.py
+++ apps/players/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Player
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "birth_date",
+        "active",
+    )
+
+    search_fields = (
+        "first_name",
+        "last_name",
+    )
+
+    list_filter = (
+        "active",
+    )
